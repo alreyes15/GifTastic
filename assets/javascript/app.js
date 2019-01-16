c2d7d4cd32ed3436c9ee306a6b43c2e5df8b5894
Alexis Reyes <Alex@Alexiss-MacBook-Pro-2.local>
2019-01-16 11:08:04 -0600

    Updated JS and HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,73 @@
 //Welcome to my take on gifTastic
 //First, variables must be storted 
-var tvShows = ["Game of Thrones", "Rick and Morty" ]
+
+var shows = ["Game of Thrones", "Rick and Morty", "The Good Doctor","The Big Bang Theory", "Empire", "South Park", "Narcos", "Cops","You","Brooklyn Nine-Nine", "Fresh Prince of Bel Air "  ]
 
 function renderButtons() {
-    $("#tvShows-view").empty();
-    for (var i = 0; i < tvShows.length; i++) {
+    $("#buttonsView").empty();
+    for (var i = 0; i < shows.length; i++) {
         var a = $("<button>");
-        a.addClass("movie");
-        a.attr("data-name", tvShows[i]);
-        a.text(tvShows[i]);
-        $("#tvShow-view").append(a);
+        a.addClass("show");
+        a.attr("data-name",shows[i]);
+        a.text(shows[i]);
+        $("#buttonsView").append(a);
     }
 }
-  $("#aad-tvShow").on("click",function(event) {
-      event.preventDefault();
+  $("#addShow").on("click",function() {
       
       var show = $("#show-input").val().trim();
-      tvShows.push(tvShows);
+      shows.push(show);
       renderButtons();
-  });
 
-  renderButtons
+      return false;
+
+  })
+ function displayGifs(){
+    var show = $(this).attr("data-name");
+    var jqueryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=dc6zaTOxFJmzC&limit=10";
+    
+    $.ajax({ url: jqueryURL, method: "GET"})
+
+      .done(function(response) {
+          console.log(response.data);
+          var results = response.data; 
+
+    for (var i = 0; i < results.length; i++) {
+     var gifDiv = $("<div class=gifs>");
+     var showGif = $("<img>")
+
+     showGif.attr("src", results[i].images.fixed_height_still.url);
+     showGif.attr("title", "Rating: " + results[i].rating);
+        showGif.attr("data-still", results[i].images.fixed_height_still.url);
+        showGif.attr("data-state", "still");
+        showGif.addClass("gif");
+        showGif.attr("data-animate", results[i].images.fixed_height.url);
+
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+
+        
+        gifDiv.append(showGif)
+        
+        $("#gifsView").prepend(gifDiv);
+      }
+
+    });
+ }
+ $(document).on('click', '.gif', function(){
+     var state = $(this).attr('data-state');
+        if ( state == 'still'){
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate')
+        }else{
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        };
+    
+});
+
+// function for displaying show gifs
+$(document).on("click", ".show", displayGifs);
+
+// initially calls the makeButtons function
+renderButtons();
