ab6f2962d2ccd69c1df2a4a2d35aec1ba51de5bc
Alexis Reyes <Alex@Alexiss-MacBook-Pro-2.local>
2019-01-16 18:26:04 -0600

    Adding CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,9 @@
 var shows = ["Game of Thrones", "Rick and Morty", "The Good Doctor","The Big Bang Theory", "Empire", "South Park", "Narcos", "Cops","You","Brooklyn Nine-Nine", "Fresh Prince of Bel Air "  ]
 
 function renderButtons() {
+
     $("#buttonsView").empty();
+
     for (var i = 0; i < shows.length; i++) {
         var a = $("<button>");
         a.addClass("show");
@@ -17,27 +19,32 @@ function renderButtons() {
       
       var show = $("#show-input").val().trim();
       shows.push(show);
+
       renderButtons();
 
       return false;
 
   })
  function displayGifs(){
+
     var show = $(this).attr("data-name");
     var jqueryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=dc6zaTOxFJmzC&limit=10";
     
-    $.ajax({ url: jqueryURL, method: "GET"})
+    $.ajax({ 
+        url: jqueryURL,
+        method: "GET"})
 
       .done(function(response) {
           console.log(response.data);
           var results = response.data; 
 
     for (var i = 0; i < results.length; i++) {
+
      var gifDiv = $("<div class=gifs>");
      var showGif = $("<img>")
 
-     showGif.attr("src", results[i].images.fixed_height_still.url);
-     showGif.attr("title", "Rating: " + results[i].rating);
+        showGif.attr("src", results[i].images.fixed_height_still.url);
+        showGif.attr("title", "Rating: " + results[i].rating);
         showGif.attr("data-still", results[i].images.fixed_height_still.url);
         showGif.attr("data-state", "still");
         showGif.addClass("gif");
@@ -50,23 +57,30 @@ function renderButtons() {
         gifDiv.append(showGif)
         
         $("#gifsView").prepend(gifDiv);
+
       }
 
     });
  }
+
  $(document).on('click', '.gif', function(){
+
      var state = $(this).attr('data-state');
+
         if ( state == 'still'){
             $(this).attr('src', $(this).data('animate'));
             $(this).attr('data-state', 'animate')
+
         }else{
             $(this).attr('src', $(this).data('still'));
             $(this).attr('data-state', 'still');
+
         };
     
 });
 
 // function for displaying show gifs
+
 $(document).on("click", ".show", displayGifs);
 
 // initially calls the makeButtons function
